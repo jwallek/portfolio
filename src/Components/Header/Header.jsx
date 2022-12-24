1d6e11b7ad92bd50145bd30dab1c04d6924de8e1
Joseph Wallek <jwallek@gmail.com>
2022-12-23 17:38:34 -0800

    added resume

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './Header.css'
 import { BsEyeglasses } from 'react-icons/bs'
 
+
 function Header() {
   return (
     <div className='header-container'>
         <div className="header-logo" data-aos="fade-up-right" data-aos-duration="1500">JW</div>
-        <div className="header-resume" data-aos="fade-up-left" data-aos-duration="1500"><button>RESUME</button></div>
+        <button className="header-resume" data-aos="fade-up-left" data-aos-duration="1500"><a href={'./Resume2022.pdf'} target={"_blank"} rel={"noreferrer"} >RESUME</a></button>
     </div>
   )
 }
